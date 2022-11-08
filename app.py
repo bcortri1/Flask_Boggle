d3e9a01fdaf60e00582268529039a1c8a5e607f4
Blake C <bcortri1@kent.edu>
2022-11-08 14:40:08 -0500

    adding templates and imports

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from boggle import Boggle
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Secret Code"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 
 boggle_game = Boggle()
